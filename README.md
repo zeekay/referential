be07c731128b7518e93c6bbdcea83135ab1a92c5
Zach Kelling <z@zeekay.io>
2015-12-07 16:27:08 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# referential  [![Build Status][travis-image]][travis-url] [![Coverage Status][coveralls-image]][coveralls-url] [![NPM version][npm-image]][npm-url]  [![Gitter chat][gitter-image]][gitter-url]
+
+Reactive immutable data structures you can share.
+
+## Install
+```bash
+$ npm install referential
+```
+
+[referential.js]: https://cdn.rawgit.com/zeekay/referential/v0.0.1/referential.min.js
+[travis-url]: https://travis-ci.org/zeekay/referential
+[travis-image]: https://img.shields.io/travis/zeekay/referential.svg
+[coveralls-url]: https://coveralls.io/r/zeekay/referential/
+[coveralls-image]: https://img.shields.io/coveralls/zeekay/referential.svg
+[npm-url]: https://www.npmjs.com/package/referential
+[npm-image]: https://img.shields.io/npm/v/referential.svg
+[downloads-image]: https://img.shields.io/npm/dm/referential.svg
+[downloads-url]: http://badge.fury.io/js/referential
+[gitter-url]: https://gitter.im/zeekay/referential
+[gitter-image]: https://img.shields.io/badge/gitter-join_chat-brightgreen.svg
---
+++ referential.js
@@ -0,0 +1,74 @@
+(function (global) {
+  var process = {
+    title: 'browser',
+    browser: true,
+    env: {},
+    argv: [],
+    nextTick: function (fn) {
+      setTimeout(fn, 0)
+    },
+    cwd: function () {
+      return '/'
+    },
+    chdir: function () {
+    }
+  };
+  // Require module
+  function require(file, callback) {
+    if ({}.hasOwnProperty.call(require.cache, file))
+      return require.cache[file];
+    // Handle async require
+    if (typeof callback == 'function') {
+      require.load(file, callback);
+      return
+    }
+    var resolved = require.resolve(file);
+    if (!resolved)
+      throw new Error('Failed to resolve module ' + file);
+    var module$ = {
+      id: file,
+      require: require,
+      filename: file,
+      exports: {},
+      loaded: false,
+      parent: null,
+      children: []
+    };
+    var dirname = file.slice(0, file.lastIndexOf('/') + 1);
+    require.cache[file] = module$.exports;
+    resolved.call(module$.exports, module$, module$.exports, dirname, file);
+    module$.loaded = true;
+    return require.cache[file] = module$.exports
+  }
+  require.modules = {};
+  require.cache = {};
+  require.resolve = function (file) {
+    return {}.hasOwnProperty.call(require.modules, file) ? require.modules[file] : void 0
+  };
+  // define normal static module
+  require.define = function (file, fn) {
+    require.modules[file] = fn
+  };
+  require.waiting = {};
+  // define asynchrons module
+  require.async = function (url, fn) {
+    require.modules[url] = fn;
+    while (cb = require.waiting[url].shift())
+      cb(require(url))
+  };
+  // Load module asynchronously
+  require.load = function (url, cb) {
+    var script = document.createElement('script'), existing = document.getElementsByTagName('script')[0], callbacks = require.waiting[url] = require.waiting[url] || [];
+    // we'll be called when asynchronously defined.
+    callbacks.push(cb);
+    // load module
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = url;
+    existing.parentNode.insertBefore(script, existing)
+  };
+  // source: src/browser.coffee
+  require.define('./browser', function (module, exports, __dirname, __filename) {
+  });
+  require('./browser')
+}.call(this, this))//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIvc3JjIn0=
