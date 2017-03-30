11617d491c78bc7d87e767ee249ba7c3e0c5b1ff
Zach Kelling <z@zeekay.io>
2017-03-30 14:39:53 -0500

    Add lockfile.

--- referential.js
+++ referential.js
@@ -1,1 +1,270 @@
-var Referential=function(){"use strict";var t,e,n,r,i,u,l=[].slice;t=Object.getOwnPropertySymbols,e=Object.prototype.hasOwnProperty,r=Object.prototype.propertyIsEnumerable,u=function(t){if(null===t||void 0===t)throw new TypeError("Object.assign cannot be called with null or undefined");return Object(t)},i=function(){var t,e,n,r,i,u,l,o,h,s;try{if(!Object.assign)return!1;if(o=new String("abc"),o[5]="de","5"===Object.getOwnPropertyNames(o)[0])return!1;for(h={},t=e=0;e<=9;t=++e)h["_"+String.fromCharCode(t)]=t;if(u=Object.getOwnPropertyNames(h).map(function(t){return h[t]}),"0123456789"!==u.join(""))return!1;for(s={},l="abcdefghijklmnopqrst".split(""),n=0,r=l.length;n<r;n++)i=l[n],s[i]=i;return"abcdefghijklmnopqrst"===Object.keys(Object.assign({},s)).join("")}catch(t){return t,!1}};var o,h,s,a,c=n=function(){return i()?Object.assign:function(){var n,i,o,h,s,a,c,f,g,p,b,d;for(b=arguments[0],g=2<=arguments.length?l.call(arguments,1):[],d=u(b),i=0,s=g.length;i<s;i++){f=g[i],n=Object(f);for(h in n)e.call(n,h)&&(d[h]=n[h]);if(t)for(c=t(n),o=0,a=c.length;o<a;o++)p=c[o],r.call(n,p)&&(d[p]=n[p])}return d}}();h=Object.prototype,s=h.hasOwnProperty,a=h.toString,"function"==typeof Symbol&&Symbol.prototype.valueOf;var f,g,p=function(t){return!!t&&!d(t)&&s.call(t,"length")&&isFinite(t.length)&&v(t.length)&&t.length>=0},b=Array.isArray||function(t){return"[object Array]"===a.call(t)},d=function(t){return"[object Boolean]"===a.call(t)},v=(o=function(t){var e;return"undefined"!=typeof window&&t===window.alert||("[object Function]"===(e=a.call(t))||"[object GeneratorFunction]"===e||"[object AsyncFunction]"===e)},function(t){return"[object Number]"===a.call(t)}),y=function(t){return"[object Object]"===a.call(t)},_=function(t){t=t||{};var e={},n=Array.prototype.slice;return Object.defineProperties(t,{on:{value:function(n,r){return"function"==typeof r&&(e[n]=e[n]||[]).push(r),t},enumerable:!1,writable:!1,configurable:!1},off:{value:function(n,r){if("*"!=n||r)if(r)for(var i,u=e[n],l=0;i=u&&u[l];++l)i==r&&u.splice(l--,1);else delete e[n];else e={};return t},enumerable:!1,writable:!1,configurable:!1},one:{value:function(e,n){function r(){t.off(e,r),n.apply(t,arguments)}return t.on(e,r)},enumerable:!1,writable:!1,configurable:!1},trigger:{value:function(r){var i,u,l,o=arguments.length-1,h=new Array(o);for(l=0;l<o;l++)h[l]=arguments[l+1];for(i=n.call(e[r]||[],0),l=0;u=i[l];++l)u.apply(t,h);return e["*"]&&"*"!=r&&t.trigger.apply(t,["*",r].concat(h)),t},enumerable:!1,writable:!1,configurable:!1}}),t},j=[].indexOf||function(t){for(var e=0,n=this.length;e<n;e++)if(e in this&&this[e]===t)return e;return-1};g=function(){var t;return t=0,function(){return t++}}();var m,w,O=f=class t{constructor(t,e,n){this._value=t,this.parent=e,this.key=n,this._cache={},this._children={},this._numChildren=0,this._id=g(),null!=this.parent&&(this.parent._children[this._id]=this,this.parent._numChildren++),_(this)}_mutate(t){var e,n,r;this._cache={},r=this._children;for(n in r)e=r[n],e._mutate();return this}destroy(){var t,e,n;n=this._children;for(e in n)t=n[e],t.destroy();return delete this._cache,delete this._children,this.off("*"),this.parent&&(delete this.parent._children[this._id],this.parent._numChildren--),this}value(t){return this.parent?null!=t?this.parent.set(this.key,t):this.parent.get(this.key):(null!=t&&(this._value=t),this._value)}ref(e){return e?new t(null,this,e):this}get(t){return t?this._cache[t]?this._cache[t]:this._cache[t]=this.index(t):this.value()}set(t,e){var n,r,i;if(y(t)){for(n in t)i=t[n],this.set(n,i);return this}return r=this.get(t),this._mutate(t),null==e?this.value(c(this.value(),t)):this.index(t,e),this._triggerSet(t,e,r),this._triggerSetChildren(t,e,r),this}_triggerSetChildren(t,e,n){var r,i,u,l,o,h,s,a,c,f,g,p;if(0===this._numChildren)return this;t+="",s=t.split("."),a="",i=[],g={};for(l in s)h=s[l],""===a?a=h:a+="."+h,i[l]=a,g[a]=new RegExp("^"+a+".?");c=this._children,p=[];for(o in c)r=c[o],f=r.key,j.call(i,f)>=0?(u=t.replace(g[r.key],""),r.trigger("set",u,e,n),p.push(r._triggerSetChildren(u,e,n))):p.push(void 0);return p}_triggerSet(t,e,n){var r;if(this.trigger("set",t,e,n),this.parent)return r=this.key+"."+t,this.parent._triggerSet(r,e,n)}extend(t,e){var n;return this._mutate(t),null==e?this.value(c(this.value(),t)):y(e)?this.value(c(this.ref(t).get(),e)):(n=this.clone(),this.set(t,e),this.value(c(n.get(),this.value()))),this}clone(e){return new t(c({},this.get(e)))}index(t,e,n=this.value(),r){var i,u,l;if(this.parent)return this.parent.index(this.key+"."+t,e);if(v(t)&&(t=String(t)),l=t.split("."),null!=e)for(;u=l.shift();){if(!l.length)return n[u]=e;i=l[0],null==n[u]&&(isNaN(Number(i))?null==n[u]&&(n[u]={}):null==n[u]&&(n[u]=[])),n=n[u]}else for(;u=l.shift();){if(!l.length)return null!=n?n[u]:void 0;n=null!=n?n[u]:void 0}}};return m=["extend","get","index","ref","set","value","on","off","one","trigger"],w=function(t,e=null){var n,r,i,u,l;for(null==e&&(e=new O(t)),l=function(t){return e.get(t)},n=function(t){return l[t]=function(){return e[t].apply(e,arguments)}},r=0,i=m.length;r<i;r++)u=m[r],n(u);return l.refer=function(t){return w(null,e.ref(t))},l.clone=function(t){return w(null,e.clone(t))},l},w.Ref=O,w}();
+var Referential = (function (objectAssign,esIs,observable) {
+'use strict';
+
+objectAssign = 'default' in objectAssign ? objectAssign['default'] : objectAssign;
+observable = 'default' in observable ? observable['default'] : observable;
+
+// src/ref.coffee
+var Ref;
+var nextId;
+var indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+nextId = (function() {
+  var ids;
+  ids = 0;
+  return function() {
+    return ids++;
+  };
+})();
+
+var Ref$1 = Ref = (function() {
+  function Ref(_value, parent, key1) {
+    this._value = _value;
+    this.parent = parent;
+    this.key = key1;
+    this._cache = {};
+    this._children = {};
+    this._numChildren = 0;
+    this._id = nextId();
+    if (this.parent != null) {
+      this.parent._children[this._id] = this;
+      this.parent._numChildren++;
+    }
+    observable(this);
+    this;
+  }
+
+  Ref.prototype._mutate = function(key) {
+    var child, id, ref;
+    this._cache = {};
+    ref = this._children;
+    for (id in ref) {
+      child = ref[id];
+      child._mutate();
+    }
+    return this;
+  };
+
+  Ref.prototype.destroy = function() {
+    var child, id, ref;
+    ref = this._children;
+    for (id in ref) {
+      child = ref[id];
+      child.destroy();
+    }
+    delete this._cache;
+    delete this._children;
+    this.off('*');
+    if (this.parent) {
+      delete this.parent._children[this._id];
+      this.parent._numChildren--;
+    }
+    return this;
+  };
+
+  Ref.prototype.value = function(state) {
+    if (!this.parent) {
+      if (state != null) {
+        this._value = state;
+      }
+      return this._value;
+    }
+    if (state != null) {
+      return this.parent.set(this.key, state);
+    } else {
+      return this.parent.get(this.key);
+    }
+  };
+
+  Ref.prototype.ref = function(key) {
+    if (!key) {
+      return this;
+    }
+    return new Ref(null, this, key);
+  };
+
+  Ref.prototype.get = function(key) {
+    if (!key) {
+      return this.value();
+    } else {
+      if (this._cache[key]) {
+        return this._cache[key];
+      }
+      return this._cache[key] = this.index(key);
+    }
+  };
+
+  Ref.prototype.set = function(key, value) {
+    var k, oldValue, v;
+    if (esIs.isObject(key)) {
+      for (k in key) {
+        v = key[k];
+        this.set(k, v);
+      }
+      return this;
+    }
+    oldValue = this.get(key);
+    this._mutate(key);
+    if (value == null) {
+      this.value(objectAssign(this.value(), key));
+    } else {
+      this.index(key, value);
+    }
+    this._triggerSet(key, value, oldValue);
+    this._triggerSetChildren(key, value, oldValue);
+    return this;
+  };
+
+  Ref.prototype._triggerSetChildren = function(key, value, oldValue) {
+    var child, childKeys, childRemainderKey, i, id, keyPart, keyParts, partialKey, ref, ref1, regExps, results;
+    if (this._numChildren === 0) {
+      return this;
+    }
+    key = key + '';
+    keyParts = key.split('.');
+    partialKey = '';
+    childKeys = [];
+    regExps = {};
+    for (i in keyParts) {
+      keyPart = keyParts[i];
+      if (partialKey === '') {
+        partialKey = keyPart;
+      } else {
+        partialKey += '.' + keyPart;
+      }
+      childKeys[i] = partialKey;
+      regExps[partialKey] = new RegExp('^' + partialKey + '\.?');
+    }
+    ref = this._children;
+    results = [];
+    for (id in ref) {
+      child = ref[id];
+      if (ref1 = child.key, indexOf.call(childKeys, ref1) >= 0) {
+        childRemainderKey = key.replace(regExps[child.key], '');
+        child.trigger('set', childRemainderKey, value, oldValue);
+        results.push(child._triggerSetChildren(childRemainderKey, value, oldValue));
+      } else {
+        results.push(void 0);
+      }
+    }
+    return results;
+  };
+
+  Ref.prototype._triggerSet = function(key, value, oldValue) {
+    var parentKey;
+    this.trigger('set', key, value, oldValue);
+    if (this.parent) {
+      parentKey = this.key + '.' + key;
+      return this.parent._triggerSet(parentKey, value, oldValue);
+    }
+  };
+
+  Ref.prototype.extend = function(key, value) {
+    var clone;
+    this._mutate(key);
+    if (value == null) {
+      this.value(objectAssign(this.value(), key));
+    } else {
+      if (esIs.isObject(value)) {
+        this.value(objectAssign((this.ref(key)).get(), value));
+      } else {
+        clone = this.clone();
+        this.set(key, value);
+        this.value(objectAssign(clone.get(), this.value()));
+      }
+    }
+    return this;
+  };
+
+  Ref.prototype.clone = function(key) {
+    return new Ref(objectAssign({}, this.get(key)));
+  };
+
+  Ref.prototype.index = function(key, value, obj, prev) {
+    var next, prop, props;
+    if (obj == null) {
+      obj = this.value();
+    }
+    if (this.parent) {
+      return this.parent.index(this.key + '.' + key, value);
+    }
+    if (esIs.isNumber(key)) {
+      key = String(key);
+    }
+    props = key.split('.');
+    if (value == null) {
+      while (prop = props.shift()) {
+        if (!props.length) {
+          return obj != null ? obj[prop] : void 0;
+        }
+        obj = obj != null ? obj[prop] : void 0;
+      }
+      return;
+    }
+    while (prop = props.shift()) {
+      if (!props.length) {
+        return obj[prop] = value;
+      } else {
+        next = props[0];
+        if (obj[prop] == null) {
+          if (isNaN(Number(next))) {
+            if (obj[prop] == null) {
+              obj[prop] = {};
+            }
+          } else {
+            if (obj[prop] == null) {
+              obj[prop] = [];
+            }
+          }
+        }
+      }
+      obj = obj[prop];
+    }
+  };
+
+  return Ref;
+
+})();
+
+// src/index.coffee
+var methods;
+var refer;
+
+methods = ['extend', 'get', 'index', 'ref', 'set', 'value', 'on', 'off', 'one', 'trigger'];
+
+refer = function(state, ref) {
+  var fn, i, len, method, wrapper;
+  if (ref == null) {
+    ref = null;
+  }
+  if (ref == null) {
+    ref = new Ref$1(state);
+  }
+  wrapper = function(key) {
+    return ref.get(key);
+  };
+  fn = function(method) {
+    return wrapper[method] = function() {
+      return ref[method].apply(ref, arguments);
+    };
+  };
+  for (i = 0, len = methods.length; i < len; i++) {
+    method = methods[i];
+    fn(method);
+  }
+  wrapper.refer = function(key) {
+    return refer(null, ref.ref(key));
+  };
+  wrapper.clone = function(key) {
+    return refer(null, ref.clone(key));
+  };
+  return wrapper;
+};
+
+refer.Ref = Ref$1;
+
+var refer$1 = refer;
+
+return refer$1;
+
+}(objectAssign,esIs,observable));
